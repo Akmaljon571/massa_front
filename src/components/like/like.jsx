4664a9eb0a7305dc1eb1b3d1473f7e3ceb4d024d
Akmaljon571 <akmaljon079@gmail.com>
2023-09-27 18:27:54 +0300

    like full finish

--- src/components/like/like.jsx
+++ src/components/like/like.jsx
@@ -3,6 +3,8 @@ import vector from '../../img/Vector 255 OQ.svg'
 import { api } from '../../content/start';
 import { useNavigate } from 'react-router-dom'
 import Err from '../../html/err';
+import img from '../../img/rasm6.png'
+import activeLike from '../../img/active-like.svg'
 import './like.scss'
 
 function HeroLike() {
@@ -13,20 +15,63 @@ function HeroLike() {
 
     useEffect(() => {
         if (token) {
-            fetch(api + 'user/like')
+            fetch(api + 'user/like', {
+                headers: {
+                    "autharization": token,
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+            })
             .then(re => re.json())
             .then(data => setLike(data))
+        } else {
+            setLike([])
         }
     }, [setLike, token]);
+
+    const clickLike = (e, find) => {
+        const _class = e.target.className.split(' ')[0]
+
+        if (_class === 'likeActive') {
+            fetch(api + `user/like/${find.like_id}`, {
+                method: 'DELETE',
+                headers: {
+                    "autharization": token,
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(re => {
+                window.location.reload(true)
+            })
+        } else {
+            navigate('/product/' + find._id)
+        }
+    }
 
     return (  
         <section className="like">
             <ul className={openCategory ? "top" : 'none'}>
                 {like?.length ?
                     like.map((e, i) => (
-                        <li onClick={() => navigate(`/product/${e._id}`)} key={i}>
-                            {e.title}
-                        </li>
+                        <li onClick={(t) => clickLike(t, e)} key={i} className="like-card">
+                        {e._id.split('').reverse().find(w => Number(w)) % 4 === 0 ? 
+                            <span className="new">New</span> 
+                        : null}
+                        {e.chegirma && e._id.split('').reverse().find(w => Number(w)) % 4 !== 0 ?
+                            <span className="chegirma">{e.chegirma}</span>
+                         : null}
+                        <div className="like-top">
+                            <div className="likeActive hear">
+                                <img className="likeActive" width={22} height={22} src={activeLike} alt="Active Like" />
+                            </div>
+                            <img className="main-img" width={265} height={265} src={img} alt="Icon" />
+                        </div>
+                        <div className="like-bottom">
+                            <h3>{e.title}</h3>
+                            <p className={e.chegirma ? 'del' : ""}>{e.price} uzs {e.chegirma && <del>{e.oldPrice} uzs</del>}</p>
+                        </div>
+                    </li>
                     ))
                 : <Err />}
             </ul>
--- src/html/err.jsx
+++ src/html/err.jsx
@@ -2,7 +2,7 @@ import img from '../img/error-6482984_960_720.webp'
 
 function Err() {
     return (  
-        <img width={300} style={{marginLeft: '500px', marginTop: "-82px"}} src={img} alt="Error page" />
+        <img width={400} style={{marginLeft: '450px', marginTop: "-130px"}} src={img} alt="Error page" />
     );
 }
 
